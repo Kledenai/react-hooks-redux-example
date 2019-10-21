cd39cac1dd3b76c96550ccc6bba123711922aea1
kledenai <bruno.silva1643@etec.sp.gov.br>
2019-10-21 19:12:27 -0300

    configurando o redux

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React from "react";
+import { Provider } from "react-redux";
+import store from "./store";
+import CourseList from "./components/courseList";
 
 function App() {
   return (
-    <p>Hello</p>
+    <Provider store={store}>
+      <CourseList />
+    </Provider>
   );
 }
 
---
+++ src/components/courseList/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Container } from "./styles";
+
+export default function CourseList() {
+  return (
+    <Container>
+      <p>Oe</p>
+    </Container>
+  );
+}
